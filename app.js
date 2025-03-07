d6d87afd4327efc8db968c0dd00647f88cb6435c
Susana Salazar <susanacst098@gmail.com>
2025-03-06 23:32:33 -0600

    add: función para agregar amigos a la lista
    valida que el nombre este vacio

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 let amigos = [];
 
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre == "") {
+        alert("Ingresa un nombre para continuar.");
+    }
+    else {
+        amigos.push(nombre);
+        alert("Nombre agregado a la lista.");
+        document.getElementById("amigo").value = "";
+    }
+}
