38fb55a65950de4b6246e0422e3a08186506d799
Susana Salazar <susanacst098@gmail.com>
2025-03-07 15:43:24 -0600

    función para actualizar la lista de amigos
    limpia el campo de texto

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 let amigos = [];
 
 function agregarAmigo() {
-    let nombre = document.getElementById("amigo").value;
-    if (nombre == "") {
+    let nombre = document.getElementById("amigo").value.trim();
+
+    if (nombre === "") {
         alert("Ingresa un nombre para continuar.");
-    }
-    else {
+    } else {
         amigos.push(nombre);
+        actualizarLista();
         alert("Nombre agregado a la lista.");
         document.getElementById("amigo").value = "";
     }
+}
+
+function actualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
 }
